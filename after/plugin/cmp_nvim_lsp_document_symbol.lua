178c40dc3c090335f3595959b33c7f7b684367df
Pablo La Greca <pablo.lagreca@msartcontract.com>
2024-05-11 23:48:54 -0300

    fixing bug

--- after/plugin/cmp_nvim_lsp_document_symbol.lua
+++ after/plugin/cmp_nvim_lsp_document_symbol.lua
@@ -1,1 +1,1 @@
-require('cmp').register_source('nvim_lsp_workspace_symbol', require('cmp_nvim_lsp_worksace_symbol').new())
+require('cmp').register_source('nvim_lsp_workspace_symbol', require('cmp_nvim_lsp_document_symbol').new())
